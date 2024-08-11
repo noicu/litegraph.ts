7586b64b4a4a9586e918e16a4584e055a9fd4417
cv <1018496570@qq.com>
2024-08-12 00:08:56 +0800

    threejs  相关节点

---
+++ packages/nodes-three/src/UpdateEvent.ts
@@ -0,0 +1,34 @@
+import {
+  BuiltInSlotType,
+  LGraphNode,
+  LiteGraph,
+  SlotLayout,
+} from "@litegraph-ts/core";
+
+
+export default class UpdateEvent extends LGraphNode {
+  static slotLayout: SlotLayout = {
+    inputs: [],
+    outputs: [
+      { name: "on_tick", type: BuiltInSlotType.EVENT },
+    ],
+  }
+
+  override onDrawForeground(ctx: CanvasRenderingContext2D) {
+
+  }
+
+  override onDrawBackground(ctx: CanvasRenderingContext2D) {
+
+  };
+
+  override onExecute(ctx: CanvasRenderingContext2D) {
+  };
+}
+
+LiteGraph.registerNodeType({
+  class: UpdateEvent,
+  title: "Update Event",
+  desc: "Update Event",
+  type: "three/update_event"
+})
---
+++ packages/nodes-three/src/index.ts
@@ -0,0 +1,1 @@
+export { default as UpdateEvent } from "./UpdateEvent";
